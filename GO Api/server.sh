1e987083d598289c93dbdaac22464964ab46ed6f
ragavanradhakrishnan <ragavan.radhakrishnan@engro.io>
2023-12-22 13:14:10 +0530

    server IP change

--- GO Api/server.sh
+++ GO Api/server.sh
@@ -1,5 +1,5 @@
 export SERVICE_PORT=3000 \
-export AI_URL="http://34.123.104.93:5000/process_video" \
+export AI_URL="http://35.222.121.103:5000/process_video" \
 export VIDEO_OUTPUT_FOLDER="Output_Video_Folder" \
 export BUCKET_NAME="encode_project_pixelpower" \
 
